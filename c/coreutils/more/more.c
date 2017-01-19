f5f517432537798b8ea6785e20438f539f8be719
Dmitriy Shilin <sdadeveloper@gmail.com>
2017-01-19 07:19:24 +0300

    coreutils-more: add ability to chose what to do next

--- c/coreutils/more/more.c
+++ c/coreutils/more/more.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 #define PAGELEN 24
 #define LINELEN 512
 
 void do_more(FILE *fin);
+int see_more();
 
 int main(int ac, char *av[])
 {
@@ -32,12 +34,18 @@ int main(int ac, char *av[])
 void do_more(FILE *fin)
 {
 	int line_cnt = 0;
+	int reply = 0;
+
 	char buf[LINELEN];
 
 	while (fgets(buf, LINELEN, fin)) {
 		if (line_cnt == PAGELEN) {
 			// ask user what to do.
-			return;
+			reply = see_more();
+			if (reply == 0) {
+				break;
+			}
+			line_cnt -= reply;
 		}
 
 		if (fputs(buf, stdout) == EOF) {
@@ -45,4 +53,22 @@ void do_more(FILE *fin)
 		}
 		line_cnt++;
 	}
+}
+
+int see_more()
+{
+	printf("\033[7m more? \033[m\n");
+	int c;
+
+	while ((c = getchar()) != EOF) {
+		if (c == 'q') {
+			return 0;
+		} else if (c == '\0') {
+			return PAGELEN;
+		} else if (c == '\n') {
+			return 1;
+		}
+	}
+
+	return 0;
 }
