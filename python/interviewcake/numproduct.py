c4b0a505d87b68c8bf457fe9739beff6d8b927ec
Dmitriy Shilin <sdadeveloper@gmail.com>
2017-01-28 08:21:47 +0300

    cake-product-of-ints: add time measurement for greedy algorithm

--- python/interviewcake/numproduct.py
+++ python/interviewcake/numproduct.py
@@ -40,9 +40,33 @@ def get_products_v2(data):
 
     return res
 
+def get_products_greedy(data):
+    d1 = []
+    for i, v in enumerate(data[:len(data) - 1]):
+        if len(d1) == 0:
+            d1.append(v)
+        else:
+            d1.append(d1[len(d1)-1] * v)
+
+    k = 1
+    for i, v in reversed(list(enumerate(data[2:]))):
+        k *= v
+        d1[i] = d1[i] * k
+
+    if len(data) == 1:
+        d1.insert(0, data[0] * k)
+    else:
+        d1.insert(0, data[1] * k)
+
+    return d1
+
 if __name__=='__main__':
     from timeit import Timer
-    t_slow = Timer("get_products_slow([1, 0, 0, 0, 0, 0, 0, 0, 0])", "from __main__ import get_products_slow")
-    t_fast = Timer("get_products_v2([1, 0, 0, 0, 0, 0, 0, 0, 0])", "from __main__ import get_products_v2")
-    print('slow = {0}, fast = {1}'.format(t_slow.timeit(), t_fast.timeit()))
+    t_slow = Timer("get_products_slow([1, 2, 6, 5, 9])", "from __main__ import get_products_slow")
+    t_fast = Timer("get_products_v2([1, 2, 6, 5, 9])", "from __main__ import get_products_v2")
+    t_greedy = Timer("get_products_greedy([1, 2, 6, 5, 9])", "from __main__ import get_products_greedy")
+
+    print('slow = {0}, fast = {1}, greedy = {2}'.format(t_slow.timeit(),
+                                                        t_fast.timeit(),
+                                                        t_greedy.timeit()))
 
