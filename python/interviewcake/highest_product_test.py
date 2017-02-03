44f660164b48fd32e6d8fa84a5c3ee82e8a3305a
Dmitriy Shilin <sdadeveloper@gmail.com>
2017-02-03 08:05:17 +0300

    cake-product-highest-product: add greedy solution

--- python/interviewcake/highest_product_test.py
+++ python/interviewcake/highest_product_test.py
@@ -8,10 +8,34 @@ class Helper(unittest.TestCase):
         data = sorted(data, reverse=True, key=lambda num: abs(num))
         return data[0] * data[1] * data[2]
 
+    def get_highest_product_greedy(self, data):
+        for i, v in enumerate(data):
+            if i > 2:
+                min_index = i - 1
+                min_num = data[i-1]
+                for n, num in enumerate([data[i-1], data[i-2], data[i-3]]):
+                    if abs(num) < abs(min_num):
+                        min_index = n
+                        min_num = num
+
+                if min_num < v:
+                    data[i-3], data[min_index] = data[min_index], data[i-3]
+                else:
+                    data[i-3], data[i] = data[i], data[i-3]
+
+        l = len(data)
+        return data[l-1] * data[l-2] * data[l-3]
+
     def test_get_max_profit_nlogn(self):
         self.assertEqual(6, self.get_highest_product_nlogn([1, 2, 3]))
-
         self.assertEqual(300,
                         self.get_highest_product_nlogn([-10, -10, 1, 3, 2]))
+
+    def test_get_max_profit_greedy(self):
+        self.assertEqual(6, self.get_highest_product_greedy([1, 2, 3]))
+        self.assertEqual(24, self.get_highest_product_greedy([3, 1, 2, 4]))
+        self.assertEqual(64, self.get_highest_product_greedy([4, 1, 4, 4]))
+        self.assertEqual(300,
+                        self.get_highest_product_greedy([-10, -10, 1, 3, 2]))
 
 unittest.main()
