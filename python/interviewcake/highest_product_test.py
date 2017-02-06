cd1f628ab4051e897d307180d170b12c40f95bcd
Dmitriy Shilin <sdadeveloper@gmail.com>
2017-02-06 08:09:25 +0300

    cake-product-highest-product: add another greedy algorithm (easy to understand)

--- python/interviewcake/highest_product_test.py
+++ python/interviewcake/highest_product_test.py
@@ -9,6 +9,11 @@ class Helper(unittest.TestCase):
         return data[0] * data[1] * data[2]
 
     def get_highest_product_greedy(self, data):
+        """This algorithm is more easy adopted for different variations of
+        this problem:
+            - what if we wanted the highest product of 4 items? O(n)
+            - what if we wanted the highest product of k items? O(kn)
+        """
         if len(data) < 3:
             raise IndexError('Get highest product of 3 requires at least 3 items')
 
@@ -29,6 +34,24 @@ class Helper(unittest.TestCase):
         l = len(data)
         return data[l-1] * data[l-2] * data[l-3]
 
+    def get_highest_product_greedy_v2(self, data):
+        res = 1
+        lo = data[0] * data[1]
+        hi = data[0] * data[1]
+
+        for i, _ in enumerate(data):
+            if i > 1:
+                res = max(res, hi * data[i])
+                res = max(res, lo * data[i])
+
+                lo = min(lo, data[i] * data[i-1])
+                lo = min(lo, data[i] * data[i-2])
+
+                hi = max(hi, data[i] * data[i-1])
+                hi = max(hi, data[i] * data[i-2])
+
+        return res
+
     def test_get_max_profit_nlogn(self):
         self.assertEqual(6, self.get_highest_product_nlogn([1, 2, 3]))
         self.assertEqual(300,
@@ -42,5 +65,14 @@ class Helper(unittest.TestCase):
                         self.get_highest_product_greedy([-10, -10, 1, 3, 2]))
         self.assertEqual(5000,
                         self.get_highest_product_greedy([1, 10, -5, 1, -100]))
+
+    def test_get_max_profit_greedy_v2(self):
+        self.assertEqual(6, self.get_highest_product_greedy_v2([1, 2, 3]))
+        self.assertEqual(24, self.get_highest_product_greedy_v2([3, 1, 2, 4]))
+        self.assertEqual(64, self.get_highest_product_greedy_v2([4, 1, 4, 4]))
+        self.assertEqual(300,
+                        self.get_highest_product_greedy_v2([-10, -10, 1, 3, 2]))
+        self.assertEqual(5000,
+                        self.get_highest_product_greedy_v2([1, 10, -5, 1, -100]))
 
 unittest.main()
