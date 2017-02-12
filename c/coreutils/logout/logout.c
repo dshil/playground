29426344a3063f02714d7bd75fa061bccd82a4f4
Dmitriy Shilin <sdadeveloper@gmail.com>
2017-02-12 14:47:56 +0300

    coreutils-logout: fix memore free

--- c/coreutils/logout/logout.c
+++ c/coreutils/logout/logout.c
@@ -1,11 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <fcntl.h>
 #include <utmpx.h>
 #include <string.h>
-
-static void gc(struct utmpx *rec, char *tty_name); /* free allocated memory */
 
 int main(int ac, char *av[])
 {
@@ -29,24 +26,17 @@ int main(int ac, char *av[])
 
 		if ((strcmp(rec->ut_line, curr_tty)) == 0) {
 			rec->ut_type = DEAD_PROCESS;
-			if ((rec = pututxline(rec)) == NULL) {
+			if (pututxline(rec) == NULL) {
 				perror("logout");
 				goto error;
 			}
 		}
 	}
 
-	gc(rec, tty_name);
+	endutxent(); /* close the database */
 	exit(EXIT_SUCCESS);
 
 error:
-	gc(rec, tty_name);
-	exit(EXIT_FAILURE);
-}
-
-static void gc(struct utmpx *rec, char *tty_name)
-{
-	free(rec);
-	free(tty_name);
 	endutxent(); /* close the database */
+	exit(EXIT_FAILURE);
 }
