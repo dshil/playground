ce6f823a099673acdc37aa46b972a6960b5ef485
Dmitriy Shilin <sdadeveloper@gmail.com>
2017-02-15 06:34:38 +0300

    coreutils-more: rework error handling

--- c/coreutils/more/more.c
+++ c/coreutils/more/more.c
@@ -1,12 +1,11 @@
 #include <curses.h>
-#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <term.h>
 
-static void do_more(FILE *fin, FILE *fp_tty, int nrows, int ncols);
-static size_t see_more(FILE *fp_tty, int nrows);
+static ssize_t do_more(FILE *fin, FILE *fp_tty, const int nrows, const int ncols);
+static size_t see_more(FILE *fp_tty, const int nrows);
 
 int main(int ac, char *av[])
 {
@@ -19,64 +18,70 @@ int main(int ac, char *av[])
 
 	FILE *fp_tty = fopen("/dev/tty", "r");
 	if (fp_tty == NULL) {
-		perror("can't open /dev/tty");
-
-		if (fclose(fp_tty) == EOF) {
-			perror("can't close /dev/tty");
-		}
-
-		exit(EXIT_FAILURE);
+		perror("/dev/tty");
+		goto error;
 	}
 
-	int nrows = tigetnum("lines");
-	int ncols = tigetnum("cols");
+	const int nrows = tigetnum("lines");
+	const int ncols = tigetnum("cols");
+
+	FILE *fin = NULL;
 
 	if (ac == 1) {
-		do_more(stdin, fp_tty, nrows, ncols);
+		if ((do_more(stdin, fp_tty, nrows, ncols)) == -1) {
+			perror("stdin");
+			goto error;
+		}
 	} else {
-		size_t i = 0;
-		FILE *fin = NULL;
+		int i = 0;
 
 		for (i = 1; i < ac; i++) {
+			if (av[i] == NULL)
+				continue;
+
 			fin = fopen(av[i], "r");
 			if (fin == NULL) {
-				fprintf(stderr, "can't open a file %s: %s\n", av[i],
-					strerror(errno));
-
-				if (fclose(fin) == EOF) {
-					fprintf(stderr, "can't close a file %s: %s\n", av[i],
-						strerror(errno));
-				}
-
-				if (fclose(fp_tty) == EOF) {
-					perror("can't close /dev/tty");
-				}
-
-				exit(EXIT_FAILURE);
+				perror(av[i]);
+				goto error;
 			}
 
-			do_more(fin, fp_tty, nrows, ncols);
+			if (do_more(fin, fp_tty, nrows, ncols) == -1) {
+				perror(av[i]);
+				goto error;
+			}
+
 			if (fclose(fin) == EOF) {
-				fprintf(stderr, "can't close a file %s: %s\n", av[i],
-					strerror(errno));
-
-				if (fclose(fp_tty) == EOF) {
-					perror("can't close /dev/tty");
-				}
-
-				exit(EXIT_FAILURE);
+				perror(av[i]);
+				goto error;
 			}
 		}
 	}
 
-	if (fclose(fp_tty) == EOF) {
-		perror("can't close /dev/tty");
-		exit(EXIT_FAILURE);
+	if (fp_tty != NULL) {
+		if (fclose(fp_tty) == EOF) {
+			perror("/dev/tty");
+		}
 	}
+
 	exit(EXIT_SUCCESS);
+
+error:
+	if (fin != NULL) {
+		if (fclose(fin) == EOF) {
+			perror("fin");
+		}
+	}
+
+	if (fp_tty != NULL) {
+		if (fclose(fp_tty) == EOF) {
+			perror("/dev/tty");
+		}
+	}
+
+	exit(EXIT_FAILURE);
 }
 
-static void do_more(FILE *fin, FILE *fp_tty, int nrows, int ncols)
+static ssize_t do_more(FILE *fin, FILE *fp_tty, const int nrows, const int ncols)
 {
 	size_t line_cnt = 0;
 	size_t reply = 0;
@@ -85,29 +90,46 @@ static void do_more(FILE *fin, FILE *fp_tty, int nrows, int ncols)
 	size_t linecap = 0;
 	ssize_t linelen = 0;
 
-	while ((linelen = getline(&line, &linecap, fin)) > 0) {
+	while ((linelen = getline(&line, &linecap, fin)) != EOF) {
+		if ((ferror(fin)) != 0)
+			goto do_more_error;
+
 		if (line_cnt == nrows) {
 			// ask user what to do.
 			reply = see_more(fp_tty, nrows);
-			if (reply == 0) {
+
+			if (reply == -1)
+				goto do_more_error;
+
+			if (reply == 0)
 				break;
-			}
+
 			line_cnt -= reply;
 		}
 
-		fwrite(line, linelen, 1, stdout);
+		if ((fwrite(line, linelen, 1, stdout)) != 1) {
+			goto do_more_error;
+		}
 		line_cnt++;
 	}
 
 	free(line);
+	return 0;
+
+do_more_error:
+	free(line);
+	return -1;
 }
 
-static size_t see_more(FILE *fp_tty, int nrows)
+static size_t see_more(FILE *fp_tty, const int nrows)
 {
 	fprintf(stderr, "\033[7m more? \033[m\n");
-	char c = 0;
+	int c = 0;
 
 	while ((c = getc(fp_tty)) != EOF) {
+		if ((ferror(fp_tty)) != 0)
+			return -1;
+
 		if (c == 'q') {
 			return 0;
 		} else if (c == ' ') {
