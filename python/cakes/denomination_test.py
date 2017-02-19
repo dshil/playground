4317148291e3665e1f954629973a5d0729b96a22
Dmitriy Shilin <sdadeveloper@gmail.com>
2017-02-19 10:32:24 +0300

    cake-change-money: rework the solution

--- python/cakes/denomination_test.py
+++ python/cakes/denomination_test.py
@@ -1,61 +1,38 @@
 import unittest
 
 class Helper(unittest.TestCase):
-    def get_denominations(self, den):
-        if den <= 0:
-            raise IndexError("Getting the denominations requires the value to \
-                              be at least 1")
-        table = []
+    def setUp(self):
+        self._cache = {}
 
-        for i in range(den+1):
-            if i == 0:
-                table.append(None)
-                continue
+    def get_exchange_number(self, amount_left, index, denominations):
+        key = str((amount_left, index))
+        if key in self._cache:
+            return self._cache[key]
 
-            if i == 1:
-                table.append([[1]])
-                continue
+        if len(denominations) == 0:
+            raise IndexError("Getting the exchange requires at least one  \
+                              denomination")
 
-            if i == 2:
-                table.append([[1, 1]])
-                continue
+        if amount_left < 0: return 0
+        if amount_left == 0: return 1
+        if index == len(denominations): return 0
 
-            lo = 1
-            hi = i - 1
+        curr_coin = denominations[index]
 
-            cell = []
-            while lo <= hi:
-                    cell.append([lo, hi])
-                    lo += 1
-                    hi -= 1
+        e_num = 0
+        while amount_left >= 0:
+            e_num += self.get_exchange_number(amount_left, index+1,
+                                              denominations)
+            amount_left -= curr_coin
 
-            for v in table[i-1]:
-                copy_v = list(v)
-                copy_v.append(1)
-                cell.append(copy_v)
+        self._cache[key] = e_num
 
-            table.append(cell)
+        return e_num
 
-        res = table[len(table)-1]
-        if len(res) == 1:
-            return res[0]
+    def test_get_number_for_denomination(self):
+         self.assertEqual(4, self.get_exchange_number(4, 0, [1, 2, 3]))
 
-        return res
-
-    def test_get_merging_ranges(self):
-        self.assertEqual([1], self.get_denominations(1))
-        self.assertEqual([1, 1], self.get_denominations(2))
-        self.assertEqual([[1, 2], [1, 1, 1]], self.get_denominations(3))
-
-        self.assertEqual([[1, 3], [2, 2], [1, 2, 1], [1, 1, 1, 1]],
-            self.get_denominations(4))
-
-        self.assertEqual([[1, 4], [2, 3], [1, 3, 1], [2, 2, 1], [1, 2, 1, 1],
-                          [1, 1, 1, 1, 1]], self.get_denominations(5))
-
-        self.assertEqual([[1, 5], [2, 4], [3, 3], [1, 4, 1], [2, 3, 1],
-                          [1, 3, 1, 1], [2, 2, 1, 1], [1, 2, 1, 1, 1],
-                          [1, 1, 1, 1, 1, 1]],
-                          self.get_denominations(6))
+    def test_get_number_for_denomination_none(self):
+         self.assertEqual(0, self.get_exchange_number(4, 0, [5, 6, 7]))
 
 unittest.main()
