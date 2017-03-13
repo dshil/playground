3c403dd1ceeecd566d1f93f6d0c18f85cdcfa60e
Dmitriy Shilin <sdadeveloper@gmail.com>
2017-03-13 07:42:27 +0300

    coreutils-tail: add option for read by lines

---
+++ c/coreutils/tail.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "reader.h"
+
+static int read_tail_lines(FILE *f, char *filename);
+
+static int nlines = 10;
+
+int main(int ac, char *av[])
+{
+	char *nlineval = NULL;
+	int suppress_file_name = 0;
+
+	int opt = 0;
+	while((opt = getopt(ac, av, "bcnq:")) != -1) {
+		switch(opt) {
+			case 'n': nlineval = optarg; break;
+			case 'q': suppress_file_name = 1; break;
+			default:
+				fprintf(stderr,
+						"Usage: %s [-q] [-n lines | -b blocks | -c bytes] \
+						[file ...]\n", av[0]);
+				exit(EXIT_FAILURE);
+		}
+	}
+
+	if (parse_num(nlineval, &nlines) == -1)
+		exit(EXIT_FAILURE);
+
+	struct read_config config;
+	config.read_file = read_tail_lines;
+
+	if (ac == optind) {
+		if (config.read_file(stdin, "stdin") == -1)
+			exit(EXIT_FAILURE);
+	} else {
+		config.is_print = ((ac - optind) > 1) && !suppress_file_name;
+		config.argv = av;
+		config.ac = ac;
+
+		if (read_files(&config) == -1) {
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	exit(EXIT_SUCCESS);
+}
+
+static int read_tail_lines(FILE *f, char *filename)
+{
+	int c = 0;
+	int n = 0;
+
+	if (fseek(f, 0, SEEK_END) == -1) {
+		perror(filename);
+		return -1;
+	}
+
+	do {
+		if (fseek(f, -1, SEEK_CUR) == -1) {
+			// the beginning of the file was reached, it's time to break
+			break;
+		}
+
+		c = getc(f);
+		if (ferror(f) != 0) {
+			perror(filename);
+			return -1;
+		}
+
+		if (c == '\n' && (++n == (nlines + 1)))
+			continue;
+
+		if (ungetc(c, f) == EOF) {
+			perror(filename);
+			return -1;
+		}
+
+	} while (n != (nlines+1));
+
+	while((c = getc(f)) != EOF) {
+		if (ferror(f) != 0) {
+			perror(filename);
+			return -1;
+		}
+
+		if (putchar(c) == EOF) {
+			perror(filename);
+			return -1;
+		}
+	}
+
+	return 0;
+}
