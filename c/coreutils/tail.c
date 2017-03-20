594022ea1e54d54122c8768df5572b811ed573c5
Dmitriy Shilin <sdadeveloper@gmail.com>
2017-03-20 09:00:35 +0300

    coreutils-tail: add -f option

--- c/coreutils/tail.c
+++ c/coreutils/tail.c
@@ -1,5 +1,8 @@
+#include <poll.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <unistd.h>
 #include <sys/inotify.h>
 #include "reader.h"
@@ -16,6 +19,10 @@ static int read_tail_blocks(FILE *f, char *filename);
 static int read_stdin_tail(struct read_config *conf);
 static void usage(char *prog_name);
 static int listen_file_changes(char *filename);
+static int handle_inotify_events(int ifd, char *filename);
+
+static int write_from_path(char *filepath);
+static int write_from_to(FILE *src, FILE *dst);
 
 static int nlines = 10;
 static int nbytes = -1;
@@ -71,10 +78,9 @@ int main(int ac, char *av[])
 	else
 		config.read_file = read_tail_lines;
 
-	if (ac == optind) {
+	if (ac == optind && observe_file == NULL) {
 		if (read_stdin_tail(&config) == -1)
 			exit(EXIT_FAILURE);
-
 	} else {
 		config.is_print = ((ac - optind) > 1) && !suppress_file_name;
 		config.argv = av;
@@ -86,6 +92,9 @@ int main(int ac, char *av[])
 	}
 
 	if (observe_file != NULL) {
+		if (write_from_path(observe_file) == -1)
+			exit(EXIT_FAILURE);
+
 		if (listen_file_changes(observe_file) == -1)
 			exit(EXIT_FAILURE);
 	}
@@ -254,15 +263,176 @@ static void usage(char *prog_name)
 
 static int listen_file_changes(char *filename)
 {
-	ssize_t fd = 0;
-	if ((fd = inotify_init()) == -1) {
-		perror(filename);
+	ssize_t ifd = 0;
+	ssize_t wfd = 0;
+
+	/* Create the file descriptor for accessing the inotify API */
+	if ((ifd = inotify_init1(IN_NONBLOCK)) == -1) {
+		perror("inotify_init1");
+		goto error;
+	}
+
+	/* Add a new watch for the filename */
+	if ((wfd = inotify_add_watch(ifd, filename, IN_MODIFY)) == -1) {
+		perror("inotify_add_watch");
+		goto error;
+	}
+
+	/* Prepare for polling */
+	nfds_t nfds = 2; /* number of file descriptors for polling */
+	struct pollfd fds[2];
+
+	/* Console input */
+	fds[0].fd = STDIN_FILENO;
+	fds[0].events = POLLIN;
+
+	/* Inotify input */
+	fds[1].fd = ifd;
+	fds[1].events = POLLIN;
+
+	int poll_num = 0;
+	char buf = 0;
+
+	/* Wait for inotify events and/or terminal input */
+	while(1) {
+		if ((poll_num = poll(fds, nfds, -1)) == -1) {
+			perror("poll");
+			return -1;
+		}
+
+		if (poll_num > 0) {
+			/* Console input is available. Empty stdin and continue. */
+			if (fds[0].revents & POLLIN) {
+				while (read(STDIN_FILENO, &buf, 1) > 0)
+					continue;
+			}
+
+			if (fds[1].revents & POLLIN) {
+				if (handle_inotify_events(ifd, filename) == -1)
+					goto error;
+			}
+		}
+	}
+
+	/* Remove the watch from an inotify watch list */
+	if (inotify_rm_watch(ifd, wfd) == -1) {
+		perror("inotify_rm_watch");
+		goto error;
+	}
+
+	if (close(wfd) == -1) {
+		perror("close");
+		goto error;
+	}
+
+	if (close(ifd) == -1) {
+		perror("close");
+		goto error;
+	}
+
+	return 0;
+
+error:
+	if (ifd != -1) {
+		if (close(ifd) == -1) {
+			perror("close");
+		}
+	}
+
+	if (wfd != -1) {
+		if (close(wfd) == -1) {
+			perror("close");
+		}
+	}
+
+	return -1;
+}
+
+static int handle_inotify_events(int ifd, char *filename)
+{
+	/* Some systems can't read integer variables if they are not
+	   properly aligned. On other systems, incorrect alignment may
+	   decrease performance. The buffer used for reading from the
+	   inotify file descriptor should have the same alignment as
+	   struct inotify_event.
+	*/
+	char buf[4*BUFSIZ]
+		__attribute__ ((aligned(__alignof__(struct inotify_event))));
+
+	const int buf_len = sizeof(buf)/sizeof(buf[0]);
+
+	const struct inotify_event *event;
+	char *ptr = NULL;
+	ssize_t len = 0;
+	int is_truncated = 0;
+
+	errno = 0;
+
+	for (;;) {
+		len = read(ifd, buf, buf_len);
+		if (len == -1 && errno != EAGAIN) {
+			perror("read");
+			return -1;
+		}
+
+		/* If the nonblocking read() found no events to read, then
+		   it returns -1 with errno set to EAGAIN. Exit the loop.
+		*/
+
+		if (len <= 0)
+			return 0;
+
+		/* Iterate over all events in the buffer */
+		for (ptr = buf; ptr < buf + len;
+				ptr += sizeof(struct inotify_event) + event->len) {
+
+			event = (const struct inotify_event *) ptr;
+
+			if (event->mask & IN_MODIFY) {
+				if (write_from_path(filename) == -1)
+					return -1;
+			}
+		}
+	}
+}
+
+static int write_from_path(char *filepath)
+{
+	FILE *f = fopen(filepath, "r");
+	if (f == NULL) {
+		perror("fopen");
+		goto error;
+	}
+
+	if (write_from_to(f, stdout) == -1)
+		goto error;
+
+	if (fclose(f) == -1) {
+		perror("fclose");
 		return -1;
 	}
 
-	if (close(fd) == -1) {
-		perror(filename);
-		return -1;
+	return 0;
+
+error:
+	if (f != NULL) {
+		if (fclose(f) == -1)
+			perror("fclose");
+	}
+	return -1;
+}
+
+static int write_from_to(FILE *src, FILE *dst)
+{
+	char buf[BUFSIZ*4];
+	const int buf_len = BUFSIZ*4;
+	ssize_t n = 0;
+
+	while ((n = fread(buf, 1, buf_len, src)) > 0) {
+		if (fwrite(buf, 1, n, dst) != n) {
+			perror("fwrite");
+			return -1;
+		}
 	}
 
 	return 0;
