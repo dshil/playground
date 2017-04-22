7810754dc31bc8d0ca68db50ea95ea2d6634f1a1
Dmitriy Shilin <sdadeveloper@gmail.com>
2017-04-22 10:15:19 +0300

    coreutils-pwd: fix path order

--- c/coreutils/pwd.c
+++ c/coreutils/pwd.c
@@ -7,65 +7,86 @@
 #include <sys/types.h>
 #include <linux/limits.h>
 
+static int pwd(void);
+static int dirwalk(ino_t ino, int (*fnc)(void));
+
 int main(int ac, char *av[])
 {
+	if (pwd() == -1)
+		exit(EXIT_FAILURE);
+
+	printf("\n");
+	exit(EXIT_SUCCESS);
+}
+
+static int pwd(void)
+{
 	struct stat chld_sb, par_sb;
-	DIR *dir = NULL;
-	struct dirent *dp = NULL;
-	char path[PATH_MAX];
-
-	for (;;) {
-		if (stat(".", &chld_sb) == -1) {
-			perror("stat");
-			exit(EXIT_FAILURE);
-		}
-
-		if (stat("..", &par_sb) == -1) {
-			perror("stat");
-			exit(EXIT_FAILURE);
-		}
-
-		if (chld_sb.st_ino == par_sb.st_ino) { /* reach the root */
-			printf("\n");
-			break;
-		}
-
-		if (chdir("..") == -1) {
-			perror("chdir");
-			exit(EXIT_FAILURE);
-		}
-
-		if ((dir = opendir(".")) == NULL) {
-			perror("opendir");
-			exit(EXIT_FAILURE);
-		}
-
-		for (;;) {
-			errno = 0;
-			dp = readdir(dir);
-			if (dp == NULL) {
-				if (!errno) {
-					if (closedir(dir) == -1) {
-						perror("closedir");
-						exit(EXIT_FAILURE);
-					}
-					break;
-				}
-
-				perror("readdir");
-				goto exit;
-			}
-
-			if (dp->d_ino == chld_sb.st_ino)
-				printf("%s/", dp->d_name);
-		}
+	if (stat(".", &chld_sb) == -1) {
+		perror("stat");
+		return -1;
 	}
 
-	exit(EXIT_SUCCESS);
+	if (stat("..", &par_sb) == -1) {
+		perror("stat");
+		return -1;
+	}
 
-exit:
+	if (chld_sb.st_ino == par_sb.st_ino) /* reach the root */
+		return 0;
+
+	if (chdir("..") == -1) {
+		perror("chdir");
+		return -1;
+	}
+
+	return dirwalk(chld_sb.st_ino, pwd);
+}
+
+static int dirwalk(ino_t ino, int (*fnc)(void))
+{
+	DIR *dir = NULL;
+	struct dirent *dp = NULL;
+
+	if ((dir = opendir(".")) == NULL) {
+		perror("opendir");
+		goto error;
+	}
+
+	for (;;) {
+		errno = 0;
+		dp = readdir(dir);
+		if (dp == NULL) {
+			if (!errno)
+				goto success;
+
+			perror("readdir");
+			goto error;
+		}
+
+		if (dp->d_ino == ino) {
+			if (fnc != NULL) {
+				if ((*fnc)() == -1)
+					goto error;
+			}
+
+			printf("/%s", dp->d_name);
+			break;
+		}
+	}
+	goto success;
+
+success:
+	if (closedir(dir) == -1) {
+		perror("closedir");
+		return -1;
+	}
+
+	return 0;
+
+error:
 	if (dir != NULL)
 		if (closedir(dir) == -1)
 			perror("closedir");
-	exit(EXIT_FAILURE);
+	return -1;
 }
