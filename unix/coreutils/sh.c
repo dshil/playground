70e5e2df031bb1286c536cbc20094ae15322b187
Dmitriy Shilin <sdadeveloper@gmail.com>
2017-05-10 17:38:30 +0300

    coreutils-sh: rework arglist building

--- unix/coreutils/sh.c
+++ unix/coreutils/sh.c
@@ -1,63 +1,50 @@
 #include <stdio.h>
+#include <errno.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <errno.h>
 #include <string.h>
-#include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/types.h>
+#include <linux/limits.h>
 
 static int execute(char **av);
+static char **init_arglist(int sz);
+static char** grow_arglist(char **al, int sz);
+static char** arglist_from_line(char *line);
+static void free_arglist(char **al);
 
 int main(int ac, char *av[])
 {
-	const int max_arg_num = 20;
-	const int max_arg_len = 100;
-	char *arglist[max_arg_num+1];
-	char *arg = NULL;
-	char buf[100];
-
-	ssize_t n = 0;
-	int curr_arg = 0;
-	int i = 0;
-	int is_exec = 0;
+	char **arglist = NULL;
+	char *line = NULL;
+	size_t cap = 0;
 
 	for (;;) {
-		if (fgets(buf, max_arg_len, stdin) == NULL) {
-			perror("fgets");
+		if (getline(&line, &cap, stdin) == -1) {
+			perror("getline");
 			goto error;
 		}
 
-		if (strncmp(buf, "exit", 4) == 0) {
-			break;
-		}
-
-		is_exec = (curr_arg == max_arg_num) || *buf == '\n';
-
-		if (is_exec) {
-			arglist[curr_arg] = NULL;
-			if (execute(arglist) == -1)
-				goto error;
-			curr_arg = 0;
+		if (strcmp(line, "\n") == 0)
 			continue;
-		}
 
-		buf[strlen(buf)-1] = '\0';
-		arg = (char *) malloc(strlen(buf) + 1);
-		if (arg == NULL) {
-			fprintf(stderr, "no memory\n");
+		if (strncmp(line, "exit", 4) == 0)
+			break;
+
+		if ((arglist = arglist_from_line(line)) == NULL)
 			goto error;
-		}
-		strcpy(arg, buf);
-		arglist[curr_arg++] = arg;
-	}
-	for (; i < curr_arg; i++)
-		free(arglist[i]);
 
+		if (execute(arglist) == -1)
+			goto error;
+
+		free_arglist(arglist);
+	}
+
+	free(line);
 	exit(EXIT_SUCCESS);
 
 error:
-	for (; i < curr_arg; i++)
-		free(arglist[i]);
+	free(line);
 	exit(EXIT_FAILURE);
 }
 
@@ -72,7 +59,7 @@ static int execute(char **av)
 	} else if (pid == 0) {
 		if (execvp(av[0], av) == -1) {
 			perror("execvp");
-			exit(EXIT_FAILURE);
+			return -1;
 		}
 	} else {
 		int chld_status = 0;
@@ -83,4 +70,77 @@ static int execute(char **av)
 	}
 
 	return 0;
+}
+
+static char** arglist_from_line(char *line)
+{
+	char *p = NULL;
+	char *q = NULL;
+	char **cp = NULL;
+	int idx = 0;
+	int sz = 1;
+	int len = 0;
+
+	char **al = init_arglist(sz);
+	if (al == NULL)
+		goto error;
+
+	for (q = line; (p = strtok(q, " ")) != NULL; q = NULL) {
+		if (idx == sz) {
+			sz *= 2;
+			if ((cp = grow_arglist(al, sz)) == NULL)
+				goto error;
+			else
+				al = cp;
+		}
+
+		len = strlen(p);
+		if (p[len-1] == '\n') {
+			strncpy(p, p, len);
+			p[len-1] = '\0';
+		}
+		al[idx++] = p;
+	}
+
+	// Grow arglist last time to set the end of the list.
+	if ((cp = grow_arglist(al, sz + 1)) == NULL)
+		goto error;
+	al = cp;
+
+	al[idx] = (char *) NULL;
+	return al;
+
+error:
+	free_arglist(al);
+	return (char **) NULL;
+}
+
+static char **init_arglist(int sz)
+{
+	char **al = (char **) malloc(sz * sizeof(char *));
+	if (al == NULL) {
+		fprintf(stderr, "malloc, no memory\n");
+		return NULL;
+	}
+	return al;
+}
+
+static char** grow_arglist(char **al, int sz)
+{
+	if ((sz  * sizeof(char *)) > ARG_MAX) {
+		fprintf(stderr, "to long args\n");
+		return (char **) NULL;
+	}
+
+	char **cp = realloc(al, sz);
+	if (cp == NULL) {
+		fprintf(stderr, "realloc, no memory\n");
+		return (char **) NULL;
+	}
+	return cp;
+}
+
+static void free_arglist(char **al)
+{
+	free(al);
 }
