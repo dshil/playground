7adf099f51db565c9c92bdf4f1d46d981d356224
Dmitriy Shilin <sdadeveloper@gmail.com>
2017-07-22 07:40:05 +0300

    coreutils-who: don't skip error

--- unix/coreutils/who.c
+++ unix/coreutils/who.c
@@ -1,32 +1,54 @@
 #include <time.h>
 #include <utmpx.h>
 #include <stdio.h>
+#include <errno.h>
+#include <string.h>
 #include <stdlib.h>
 
 int main(int ac, char *av[])
 {
-	struct utmpx *rec = NULL;
 	setutxent(); /* reset the database */
 
-	time_t tm = 0;
-	char *tm_str = NULL;
+	struct utmpx *rec = NULL;
 
-	while((rec = getutxent()) != NULL) {
+	struct tm *tm_info = NULL;
+	const int tm_len = 20;
+	char time_str [tm_len];
+	time_t tv_sec = 0;
+
+	for (;;) {
+		errno = 0;
+		rec = getutxent();
+
+		if (rec == NULL) {
+			if (errno) {
+				perror("getutxent");
+				goto error;
+			}
+			break;
+		}
+
 		if (rec->ut_type != USER_PROCESS)
 			continue;
 
-		tm = rec->ut_tv.tv_sec;
-		tm_str = 4+ctime(&tm);
-		// No need to include '\n' character.
-		tm_str[strlen(tm_str)-1] = '\0';
+		tv_sec = (time_t) rec->ut_tv.tv_sec;
+		if ((tm_info = localtime(&tv_sec)) == NULL) {
+			perror("localtime");
+			goto error;
+		}
 
+		strftime(time_str, tm_len, "%b %e %H:%M", tm_info);
 		printf("%s\t%s\t%s\t(%s)\n",
 				rec->ut_user,
 				rec->ut_line,
-				tm_str,
+				time_str,
 				rec->ut_host);
 	}
 
 	endutxent(); /* close the database */
 	exit(EXIT_SUCCESS);
+
+error:
+	endutxent();
+	exit(EXIT_FAILURE);
 }
